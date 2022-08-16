27d36549c96164c7821658e249cf412b4a54654a
Aakash Bhumbla <aakash.bhumbla@gmail.com>
2022-08-16 08:44:52 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -49,9 +49,9 @@ class App extends React.Component {
         let output = data.label;
         let color = "white"
         switch(output) {
-          case "positive": color = "black";break;
-          case "negative": color = "black";break;
-          default: color = "black";
+          case "positive": color = "green";break;
+          case "negative": color = "red";break;
+          default: color = "yellow";
         }
         output = <div style={{color:color,fontSize:20}}>{output}</div>
         this.setState({sentimentOutput:output});
@@ -67,6 +67,7 @@ class App extends React.Component {
 
     fetch(url).then((response)=>{
       response.json().then((data)=>{
+          console.log(data);
       this.setState({sentimentOutput:<EmotionTable emotions={data}/>});
   })})  ;
   }
--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -9,9 +9,12 @@ class EmotionTable extends React.Component {
           <table className="table table-bordered">
             <tbody>
             {
-              /*Write code to use the .map method that you worked on in the 
-              Hands-on React lab to extract the emotions. If you are stuck,
-              please click the instructions to see how to implement a map*/
+              Object.entries(this.props.emotions).map((ele) => {
+                  return ( <tr>
+                      <td>{ele[0]}</td>
+                      <td>{ele[1]}</td>
+                  </tr>)
+              })
             }
             </tbody>
           </table>
